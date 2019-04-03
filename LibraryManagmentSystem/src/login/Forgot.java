26e8186bd6a3e692f648d75d042517d6a74c70a0
KostaLaz <44593214+KostaLaz@users.noreply.github.com>
2019-04-03 13:20:20 +0200

    Commit

---
+++ LibraryManagmentSystem/src/login/Forgot.java
@@ -0,0 +1,116 @@
+package login;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
+
+public class Forgot extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Forgot frame = new Forgot();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Forgot() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 467, 362);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Username");
+		lblNewLabel.setFont(new Font("Verdana", Font.BOLD, 12));
+		lblNewLabel.setBounds(35, 41, 101, 22);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Name");
+		lblNewLabel_1.setFont(new Font("Verdana", Font.BOLD, 12));
+		lblNewLabel_1.setBounds(35, 89, 101, 22);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Answer");
+		lblNewLabel_2.setFont(new Font("Verdana", Font.BOLD, 12));
+		lblNewLabel_2.setBounds(35, 169, 101, 22);
+		contentPane.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("Your Password");
+		lblNewLabel_3.setFont(new Font("Verdana", Font.BOLD, 12));
+		lblNewLabel_3.setBounds(35, 209, 101, 22);
+		contentPane.add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_4 = new JLabel("Your Security Question");
+		lblNewLabel_4.setFont(new Font("Verdana", Font.BOLD, 11));
+		lblNewLabel_4.setBounds(35, 131, 149, 22);
+		contentPane.add(lblNewLabel_4);
+		
+		textField = new JTextField();
+		textField.setBounds(197, 42, 138, 22);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(197, 91, 138, 20);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(197, 133, 138, 20);
+		contentPane.add(textField_2);
+		textField_2.setColumns(10);
+		
+		textField_3 = new JTextField();
+		textField_3.setBounds(197, 171, 138, 20);
+		contentPane.add(textField_3);
+		textField_3.setColumns(10);
+		
+		textField_4 = new JTextField();
+		textField_4.setBounds(197, 211, 138, 20);
+		contentPane.add(textField_4);
+		textField_4.setColumns(10);
+		
+		JButton btnNewButton = new JButton("Search");
+		btnNewButton.setIcon(new ImageIcon(Forgot.class.getResource("/login/icons/iconfinder_Find_132785.png")));
+		btnNewButton.setBounds(345, 42, 96, 23);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_3 = new JButton("Retrive");
+		btnNewButton_3.setIcon(new ImageIcon(Forgot.class.getResource("/login/icons/iconfinder_Person_132730.png")));
+		btnNewButton_3.setBounds(345, 170, 96, 23);
+		contentPane.add(btnNewButton_3);
+		
+		JButton btnNewButton_4 = new JButton("Back");
+		btnNewButton_4.setIcon(new ImageIcon(Forgot.class.getResource("/login/icons/iconfinder_Back_132600.png")));
+		btnNewButton_4.setBounds(352, 210, 89, 23);
+		contentPane.add(btnNewButton_4);
+	}
+}
--- LibraryManagmentSystem/src/login/Login.java
+++ LibraryManagmentSystem/src/login/Login.java
@@ -15,10 +15,13 @@ import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.border.TitledBorder;
+import javax.swing.border.EtchedBorder;
+import javax.swing.UIManager;
 
 public class Login extends JFrame {
 
-	private JPanel contentPane;
+	private JPanel contentPaneLogin;
 	private JTextField textField;
 	private JTextField textField_1;
 
@@ -44,42 +47,42 @@ public class Login extends JFrame {
 	public Login() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
+		contentPaneLogin = new JPanel();
+		contentPaneLogin.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPaneLogin);
+		contentPaneLogin.setLayout(null);
 		
 		JLabel lbUser = new JLabel("Username");
 		lbUser.setFont(new Font("Verdana", Font.BOLD, 14));
 		lbUser.setBounds(50, 58, 81, 21);
-		contentPane.add(lbUser);
+		contentPaneLogin.add(lbUser);
 		
 		JLabel lblPassword = new JLabel("Password");
 		lblPassword.setFont(new Font("Verdana", Font.BOLD, 14));
 		lblPassword.setBounds(50, 121, 81, 21);
-		contentPane.add(lblPassword);
+		contentPaneLogin.add(lblPassword);
 		
 		textField = new JTextField();
 		textField.setBounds(207, 60, 141, 20);
-		contentPane.add(textField);
+		contentPaneLogin.add(textField);
 		textField.setColumns(10);
 		
 		textField_1 = new JTextField();
 		textField_1.setBounds(207, 123, 141, 20);
-		contentPane.add(textField_1);
+		contentPaneLogin.add(textField_1);
 		textField_1.setColumns(10);
 		
 		JButton btnLogin = new JButton("Login");
 		btnLogin.setIcon(new ImageIcon(Login.class.getResource("/login/icons/iconfinder_Tick_132261.png")));
 		btnLogin.setFont(new Font("Verdana", Font.BOLD, 12));
 		btnLogin.setBounds(73, 173, 104, 23);
-		contentPane.add(btnLogin);
+		contentPaneLogin.add(btnLogin);
 		
 		JButton btnSignup = new JButton("Signup");
 		btnSignup.setIcon(new ImageIcon(Login.class.getResource("/login/icons/iconfinder_Right_132313.png")));
 		btnSignup.setFont(new Font("Verdana", Font.BOLD, 12));
 		btnSignup.setBounds(246, 173, 116, 23);
-		contentPane.add(btnSignup);
+		contentPaneLogin.add(btnSignup);
 		
 		JButton btnNewButton_2 = new JButton("Forgot password?");
 		btnNewButton_2.setIcon(null);
@@ -87,25 +90,32 @@ public class Login extends JFrame {
 			public void actionPerformed(ActionEvent arg0) {
 			}
 		});
-		btnNewButton_2.setFont(new Font("Verdana", Font.BOLD, 12));
-		btnNewButton_2.setBounds(246, 215, 157, 22);
-		contentPane.add(btnNewButton_2);
+		btnNewButton_2.setFont(new Font("Verdana", Font.BOLD, 10));
+		btnNewButton_2.setBounds(185, 216, 151, 22);
+		contentPaneLogin.add(btnNewButton_2);
 		
 		JLabel lblForgotPassword = new JLabel("Trouble login...");
 		lblForgotPassword.setForeground(Color.RED);
 		lblForgotPassword.setFont(new Font("Verdana", Font.BOLD, 12));
 		lblForgotPassword.setBounds(46, 217, 129, 19);
-		contentPane.add(lblForgotPassword);
+		contentPaneLogin.add(lblForgotPassword);
 		
 		JLabel lblNewLabel = new JLabel("");
 		lblNewLabel.setIcon(new ImageIcon(Login.class.getResource("/login/icons/iconfinder_Lock_132279.png")));
 		lblNewLabel.setBounds(356, 63, 30, 19);
-		contentPane.add(lblNewLabel);
+		contentPaneLogin.add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("New label");
 		lblNewLabel_1.setIcon(new ImageIcon(Login.class.getResource("/login/icons/iconfinder_Key_132286.png")));
 		lblNewLabel_1.setBounds(358, 122, 30, 23);
-		contentPane.add(lblNewLabel_1);
+		contentPaneLogin.add(lblNewLabel_1);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(UIManager.getColor("Button.background"));
+		panel.setForeground(new Color(0, 204, 255));
+		panel.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, new Color(255, 255, 255), new Color(160, 160, 160)), "Login", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(51, 204, 204)));
+		panel.setBounds(32, 32, 370, 219);
+		contentPaneLogin.add(panel);
 		
 	}
 }
